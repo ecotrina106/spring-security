150da56c09879ce80ece02344c28b4128de84ac2
Edinson Cotrina Ramos <ecotrina106@gmail.com>
2024-08-13 22:53:43 -0500

    New Spring Security Implemetation Tecnologia Avanza

---
+++ SpringSecurityTecAvanza/src/main/java/com/app/SpringSecurityTecAvanzaApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTecAvanzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTecAvanzaApplication.class, args);
+	}
+
+}
