06b027989177ca6d051a82017684bf8ba53e72e9
Edinson Cotrina Ramos <ecotrina106@gmail.com>
2024-08-13 19:01:53 -0500

    New Spring Security Implemetation Jaax

---
+++ SpringSecurityJaax/src/main/java/com/app/SpringSecurityJaax/SpringSecurityJaaxApplication.java
@@ -0,0 +1,13 @@
+package com.app.SpringSecurityJaax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJaaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJaaxApplication.class, args);
+	}
+
+}
