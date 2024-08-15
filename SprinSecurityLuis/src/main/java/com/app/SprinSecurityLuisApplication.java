51932e5595201ad4d89991f5a6ab96ca882de5f7
Edinson Cotrina Ramos <ecotrina106@gmail.com>
2024-08-14 19:19:53 -0500

    New Spring Security Implemetation Luis marquez

---
+++ SprinSecurityLuis/src/main/java/com/app/SprinSecurityLuisApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinSecurityLuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinSecurityLuisApplication.class, args);
+	}
+
+}
